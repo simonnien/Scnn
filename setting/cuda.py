007ce2b3c9bf943688f47e08a9470a3c1564d246
learnc2n2 <learnc2n2@gmail.com>
2026-02-03 02:34:46 +0800

    CUDA setting viewer

---
+++ setting/cuda.py
@@ -0,0 +1,10 @@
+import torch
+
+# 1. 檢查 CUDA 是否可用
+if torch.cuda.is_available():
+    print("✅ 成功抓到顯卡！")
+    print(f"顯卡數量: {torch.cuda.device_count()}")
+    print(f"顯卡型號: {torch.cuda.get_device_name(0)}")
+    print(f"當前 CUDA 版本: {torch.version.cuda}")
+else:
+    print("❌ 抓不到顯卡，目前只能使用 CPU。")
